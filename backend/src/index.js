f03a10e00d121e70d8986519313e7d3752ee4201
Arhin David Kwabena <davidarhin2005@gmail.com>
2025-05-01 19:52:36 +0000

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
